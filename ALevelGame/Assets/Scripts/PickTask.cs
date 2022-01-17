e2ff8f96a74869e0e4e0e77c719157c6c8dd924a
saoirsebe <saoirse.be@gmail.com>
2022-01-17 14:54:04 +0000

    picks random object in list of objects

---
+++ ALevelGame/Assets/Scripts/PickTask.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickTask : MonoBehaviour
+{ 
+    public GameObject[] objects;
+    List<string> gameObjectNames = new List<string>();
+    private int objectToFindIndex;
+    public string objectToFind;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var item in objects)
+        {
+            string itemName = item.name;
+            gameObjectNames.Add(itemName);
+        }
+        objectToFindIndex = Random.Range(0, gameObjectNames.Count - 1);
+        objectToFind = gameObjectNames[objectToFindIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
